cc869fa5de49242f1b6298e9931d61cb004b197b
yagiriku <c0a25246a9@edu.teu.ac.jp>
2026-04-14 18:00:37 +0900

    演習問題3修正

--- flying_kokaton.py
+++ flying_kokaton.py
@@ -21,20 +21,24 @@ def main():
             if event.type == pg.QUIT: return
 
         key_lst = pg.key.get_pressed()
+        x = -1
+        y = 0
         if key_lst[pg.K_UP]:
-            kt_rct.move_ip((0, -1))
+            x += 0
+            y -= 1
         if key_lst[pg.K_DOWN]:
-            kt_rct.move_ip((0, 1))
+            x += 0
+            y += 1
         if key_lst[pg.K_LEFT]:
-            kt_rct.move_ip((-1, 0))
+            x += 0
         if key_lst[pg.K_RIGHT]:
-            kt_rct.move_ip((2, 0))
+            x += 2   
+        kt_rct.move_ip((x,y))
         x = tmr%3200
         screen.blit(bg_img, [-x, 0])#練習2
         screen.blit(bg_img_fl, [-x+1600, 0])
         screen.blit(bg_img, [-x+3200, 0])
         screen.blit(kt_img, kt_rct)
-        kt_rct.move_ip((-1, 0))
         pg.display.update()
         tmr += 1
         clock.tick(200)

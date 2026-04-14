bb99ae592791ca7d673c7d517c18e065bbf4b353
yagiriku <c0a25246a9@edu.teu.ac.jp>
2026-04-14 16:19:30 +0900

    rensyuowari

--- pygame_image.py
+++ pygame_image.py
@@ -13,19 +13,30 @@ def main():
     bg_img_fl = pg.transform.flip(bg_img, True, False)
     kt_img = pg.image.load("fig/3.png")#練習3
     kt_img = pg.transform.flip(kt_img, True, False)
+    kt_rct = kt_img.get_rect()
+    kt_rct.center = 300,200
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
+        key_lst = pg.key.get_pressed()
+        if key_lst[pg.K_UP]:
+            kt_rct.move_ip((0, -1))
+        if key_lst[pg.K_DOWN]:
+            kt_rct.move_ip((0, 1))
+        if key_lst[pg.K_LEFT]:
+            kt_rct.move_ip((-1, 0))
+        if key_lst[pg.K_RIGHT]:
+            kt_rct.move_ip((1, 0))
         x = tmr%3200
         screen.blit(bg_img, [-x, 0])#練習2
         screen.blit(bg_img_fl, [-x+1600, 0])
         screen.blit(bg_img, [-x+3200, 0])
-        screen.blit(kt_img, [300, 200])
+        screen.blit(kt_img, kt_rct)
         pg.display.update()
         tmr += 1
-        clock.tick(3200)
+        clock.tick(8000)
 if __name__ == "__main__":
     pg.init()
     main()
